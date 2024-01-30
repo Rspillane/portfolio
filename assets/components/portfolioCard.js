ee0daa1c990a6444f6868b513ce9cf9fa6807d19
Robert Spillane <robert.e.spillane@gmail.com>
2024-01-30 21:51:10 +0000

    styling of hero image

--- assets/components/portfolioCard.js
+++ assets/components/portfolioCard.js
@@ -10,14 +10,14 @@ const projects = [
     {
         id: 2,
         title: 'Stickr',
-        img: 'image-placeholder',
+        img: 'https://private-user-images.githubusercontent.com/41272270/273391163-67aad3ab-3dff-4b43-81ab-f3b09cd2b394.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY1NjQ2NzYsIm5iZiI6MTcwNjU2NDM3NiwicGF0aCI6Ii80MTI3MjI3MC8yNzMzOTExNjMtNjdhYWQzYWItM2RmZi00YjQzLTgxYWItZjNiMDljZDJiMzk0LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMjklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTI5VDIxMzkzNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWE4YWE2NWI5NmE1NDk2ZGMyYmRlMTQ5MjAzMTg1MjkwYWNkM2U4MzlhYTdiNzU3MzY5ZDcyNmFkNjhlMDA5NGMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.DFdQRPJYKEtE8_F_dooSRWRRRW_wQJiGEru5k-xlfqA',
         description: 'short description of the project',
         projectTags: ['HTML', 'CSS', 'JavaScript']
     },
     {
         id: 3,
         title: 'Project-3',
-        img: 'image-placeholder',
+        img: 'https://private-user-images.githubusercontent.com/41272270/273391163-67aad3ab-3dff-4b43-81ab-f3b09cd2b394.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDY1NjQ2NzYsIm5iZiI6MTcwNjU2NDM3NiwicGF0aCI6Ii80MTI3MjI3MC8yNzMzOTExNjMtNjdhYWQzYWItM2RmZi00YjQzLTgxYWItZjNiMDljZDJiMzk0LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMjklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTI5VDIxMzkzNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWE4YWE2NWI5NmE1NDk2ZGMyYmRlMTQ5MjAzMTg1MjkwYWNkM2U4MzlhYTdiNzU3MzY5ZDcyNmFkNjhlMDA5NGMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.DFdQRPJYKEtE8_F_dooSRWRRRW_wQJiGEru5k-xlfqA',
         description: 'short description of the project',
         projectTags: ['HTML', 'CSS', 'JavaScript']
     }
@@ -32,7 +32,6 @@ const createCard = (project) => {
     card.classList.add(`portfolio-card-${project.id}`);
 
     let contents = `
-    <a href="">
     <div class="portfolio-card">
     <h3 class="portfolio-title">${project.title}</h3>
     <div class="portfolio-image">
@@ -41,9 +40,9 @@ const createCard = (project) => {
     <p class="portfolio-descr">${project.description}</p>
     <div class="portfolio-tags">${project.projectTags[0]},${project.projectTags[1]}</div>
     <a class="button card-link"><p>Github</p></a>
-    <button class="button card-link">Live Demo</button>
+    <a class="button card-link"><p>Live Demo</p></a>
     </div> 
-    </a>`;
+    `;
     card.innerHTML = contents;
     cardContainer.append(card);
 }
--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const heroImages = document.getElementsByClassName('hero-image');
 
 const changeCard = (event) => {
     card = document.getElementById(event.target.id);
+    console.log(card);
     card.style.zIndex -= 1;
+    console.log(card);
 }
 
 for (i=0; i< heroImages.length; i++){
-    images[i].addeventlistener('click', changeCard);
+    heroImages[i].addEventListener('click', changeCard);
 }
